1000c1139a76de10acd1257934445c97803f4843
hoangtiot <dohuyhoang1120@gmail.com>
2024-05-09 19:22:54 +0700

    update IncomeController + add IncomeResDto

--- src/main/java/com/hoangtiot/report/controller/IncomeController.java
+++ src/main/java/com/hoangtiot/report/controller/IncomeController.java
@@ -1,6 +1,8 @@
 package com.hoangtiot.report.controller;
 
 import com.hoangtiot.report.dto.req.IncomeReqDto;
+import com.hoangtiot.report.dto.res.ApiResponse;
+import com.hoangtiot.report.dto.res.IncomeResDto;
 import com.hoangtiot.report.model.Income;
 import com.hoangtiot.report.model.ShiftReport;
 import com.hoangtiot.report.service.IncomeService;
@@ -13,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -25,31 +28,39 @@ public class IncomeController {
     private ShiftReportService shiftReportService;
 
     @GetMapping("/")
-    public ResponseEntity<List<Income>> findAll(){
-        return ResponseEntity.ok().body(incomeService.findAll());
+    public ApiResponse<List<IncomeResDto>> findAll(){
+        List<IncomeResDto> listDto = new ArrayList<>();
+        for (Income income : incomeService.findAll()){
+            IncomeResDto dto = new IncomeResDto();
+            dto.fromIncome(income);
+            listDto.add(dto);
+        }
+        return ApiResponse.<List<IncomeResDto>>builder().data(listDto).build();
     }
 
     @GetMapping("/by_report/{rp_id}")
-    public ResponseEntity<List<Income>> findByReport(@PathVariable @Min(1) @NotNull int rp_id) {
-        ShiftReport shiftReport = null;
-        if (shiftReportService.isExist(rp_id)) {
-            shiftReport = shiftReportService.findById(rp_id).orElse(null);
+    public ApiResponse<List<IncomeResDto>> findByReport(@PathVariable @Min(1) @NotNull int rp_id) {
+        List<IncomeResDto> listDto = new ArrayList<>();
+        for (Income income : incomeService.findByReport(shiftReportService.findById(rp_id).orElse(null))){
+            IncomeResDto dto = new IncomeResDto();
+            dto.fromIncome(income);
+            listDto.add(dto);
         }
-        return ResponseEntity.ok().body(incomeService.findByReport(shiftReport));
+        return ApiResponse.<List<IncomeResDto>>builder().data(listDto).build();
     }
 
-    @PostMapping("/add")
-    public ResponseEntity<String> add(@Valid @RequestBody IncomeReqDto incomeReqDto){
-        String rs = "Add failed";
-        Income income = new Income();
-        incomeReqDto.toIncome(income);
-        ShiftReport shiftReport = null;
-        if (shiftReportService.isExist(incomeReqDto.rp_id)) {
-            shiftReport = shiftReportService.findById(incomeReqDto.rp_id).orElse(null);
-            income.setShiftReport(shiftReport);
-        }
-        if (incomeService.addIncome(income))
-            rs = "Add " +incomeReqDto.toString()+ "succesfully";
-        return ResponseEntity.ok().body(rs);
-    }
+//    @PostMapping("/add")
+//    public ResponseEntity<String> add(@Valid @RequestBody IncomeReqDto incomeReqDto){
+//        String rs = "Add failed";
+//        Income income = new Income();
+//        incomeReqDto.toIncome(income);
+//        ShiftReport shiftReport = null;
+//        if (shiftReportService.isExist(incomeReqDto.rp_id)) {
+//            shiftReport = shiftReportService.findById(incomeReqDto.rp_id).orElse(null);
+//            income.setShiftReport(shiftReport);
+//        }
+//        if (incomeService.addIncome(income))
+//            rs = "Add " +incomeReqDto.toString()+ "succesfully";
+//        return ResponseEntity.ok().body(rs);
+//    }
 }
---
+++ src/main/java/com/hoangtiot/report/dto/res/IncomeResDto.java
@@ -0,0 +1,22 @@
+package com.hoangtiot.report.dto.res;
+
+import com.hoangtiot.report.constant.PaymentMethod;
+import com.hoangtiot.report.model.Income;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class IncomeResDto {
+    private int id;
+    private PaymentMethod paymentMethod;
+    private double amount;
+    private Date time;
+
+    public void fromIncome(Income income){
+        this.id = income.getId();
+        this.paymentMethod = income.getPaymentMethod();
+        this.amount = income.getAmount();
+        this.time = income.getShiftReport().getTime();
+    }
+}
